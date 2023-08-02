9788a8cb5ab4897a33d52655001c6867647d52b0
Joseph Nguyen <joseph.t.nguyen99@gmail.com>
2023-08-02 23:15:42 +1000

    Cleaned up names

--- Slideshow/main.js
+++ Slideshow/main.js
@@ -1,15 +1,15 @@
 "use strict";
 const panels = document.querySelectorAll(".panel");
 
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove("active")
+    });
+}
+
 panels.forEach(panel => {
     panel.addEventListener("click", () => {
         removeActiveClasses()
         panel.classList.add("active")
     });
 })
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove("active")
-    });
-}
